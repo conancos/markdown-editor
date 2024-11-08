141b8c3f8b3886167d058c841b2844fe7ac09c31
conancos <15524757+conancos@users.noreply.github.com>
2024-11-08 23:25:33 +0100

    add license

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
         <footer className="footer">
-            <p>Copyright © 2024 conan_cos - bajo licencia ---</p>
+            <p>Copyright © 2024 conan_cos - bajo licencia <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License v3.0</a></p>
         </footer>
     )
 }
