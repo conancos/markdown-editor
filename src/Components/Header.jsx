7128f2a4f3034de05f9d9d384756e8ef2e40be85
conancos <15524757+conancos@users.noreply.github.com>
2024-11-11 01:47:47 +0100

    change import for markdown2.svg and change method of render(background to img) for no problems width relative path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,26 @@
-const markdown2 = new URL('../assets/icons/markdown2.svg', import.meta.url).href
+import markdown2 from '../assets/icons/markdown2.svg'
+/* const markdown2 = new URL('../assets/icons/markdown2.svg', import.meta.url).href */
 
 const Header = () => {
 
     const svgStyle = {
-        backgroundImage: `url(${markdown2})`, 
-        backgroundPosition: "center 1px", 
-        backgroundSize: "contain", 
-        backgroundRepeat: "no-repeat",
-        width: "32px", 
-        height: "32px",
-        marginInline: ".2rem", 
+        /* backgroundImage: `url(${markdown2})`,  */
+        /* backgroundPosition: "center 1px",  */
+        /* backgroundSize: "contain",  */
+        /* backgroundRepeat: "no-repeat", */
+        paddingTop: "2px",
+        width: "42px", 
+        height: "42px",
+        marginInline: ".5rem",
+        filter: "drop-shadow(0px 0px 4px #0a142b) drop-shadow(0 0 8px #0a142b)", 
+
     }
 
     return (
         <header className="header">
             <nav><span>🔵</span><span>⚪</span><span>🟣</span></nav>
             <h1 className="title-header">Estiliza en Markdown</h1>
-            <span style={svgStyle}></span>
+            <img src={markdown2} alt="markdown icon" style={svgStyle} />
         </header> 
     )
 }
